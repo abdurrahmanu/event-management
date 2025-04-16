5272dd00ede16424fd8add581751c6820746e6b9
abdurrahmanu <ahmadabdulrahman103@gmail.com>
2025-04-16 09:20:26 +0100

    update

--- components/Category.vue
+++ components/Category.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded-lg scale:[0.8] xl:scale-[1] hover:scale-[1.05] transition-all duration-[200ms] min-w-[300px] cursor-pointer relative overflow-hidden">
+    <div class="rounded-lg scale:[0.8] xl:scale-[1] hover:scale-[1.05] transition-all duration-[200ms] cursor-pointer relative overflow-hidden">
         <NuxtImg :src="eventType" alt="" width="100%" height="100%"/>
     </div>
 </template>
